118313f5e9ebba8012dae2f09d735348e03a8bae
aliwka44 <sukurovali17@gmail.com>
2024-05-07 00:28:42 +0400

    mvc

---
+++ WebApplicationFirst/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+  
+
+namespace WebApplicationFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public async Task<IActionResult> Index()
+        {
+            return new ViewResult();
+        }
+
+        public async Task<IActionResult> Contact()
+        {
+            return new ViewResult();
+        }
+    }
+}
